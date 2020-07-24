1b7cbc4abf8b923b16a1784e7e1d35b01c4b6443
Jonathan <jonathanbanda00@gmail.com>
2020-07-24 04:37:13 -0500

    Add files via upload

---
+++ Python_introduction_basics/Python_basics.py
@@ -0,0 +1,67 @@
+"""
+Types
+
+    Integers
+    1 2 3 4 -1 -3
+    1.2 1.6
+
+    Strings
+    "Hello"
+    'bannana'
+    "ajs1233"
+
+    Booleans
+    True --> 1
+    False --> 0
+
+"""
+""""
+print(type(1))
+print(type(1.5))
+print(type("easy"))
+print(type(True))
+print(type(False))
+"""""
+
+"""
+
+Expressions
+    
+    1 + 1
+    2 / 2
+    +, -, *, ^, |, &, %
+    
+variable = 1 + 2
+
+Variables
+
+
+"""
+
+"""
+
+String operations
+    .upper()
+    .lower()
+    .replace()
+    .find()
+
+
+# Indexing
+# Negative Indexing
+# Slicing
+# Stride
+# Concatenate Strings
+# Escape Sequence
+
+
+"""
+"""
+    .upper()
+    .lower()
+    .replace()
+    .find()
+"""
+
+
+
---
+++ Python_introduction_basics/python_data_structures.py
@@ -0,0 +1,75 @@
+"""
+
+Lists
+Tuples
+
+    Ordered Sequence
+    Index
+    Concat
+    Slice
+    Mutable/ Immutable
+
+Dictionary
+Sets
+
+"""
+
+# Lists Examples
+
+firstList = [1, 2, "Three", 4]
+secondList = [5, "Six", 'Seven']
+
+"""print(X[1])
+print(X[2])
+print(X[3])"""
+
+"""concat_Lists = firstList + [4,5,6]
+
+print(concat_Lists)"""
+
+"""list = ['d', 'o', 'g']
+list2 = ["dog"]
+"""
+
+# LISTS ARE MUTABLE
+
+"""List = [1, 2, 3, 4, 5, [6, 7, 8, 9, 10]]
+
+
+print(List[5][0])
+print(List[5][1])
+print(List[5][2])
+print(List[5][3])"""
+
+
+"""
+Tuples
+
+    Ordered Sequence
+    Index
+    Concat
+    Slice
+    Immutable
+    
+"""
+
+x = ("1", 2.0, 3)
+y = (4, 5, "6")
+
+
+
+
+"""
+Dictionary
+
+"""
+
+x = {'name' : {'first': 'John', 'last': 'Doe'},
+     'jobs' : ['digger', 'manager']}
+
+
+
+print(x['jobs'][1])
+
+
+
